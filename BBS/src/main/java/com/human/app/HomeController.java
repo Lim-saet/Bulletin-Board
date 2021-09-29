5f711e66bb28acad6935711039a353826355e6d3
matsk311 <matsk311@naver.com>
2021-09-29 17:29:37 +0900

    xml,interface 파일 생성오류-servlet-context 오류

--- BBS/src/main/java/com/human/app/HomeController.java
+++ BBS/src/main/java/com/human/app/HomeController.java
@@ -6,8 +6,10 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	@Autowired
+	private SqlSession sqlSession;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -63,6 +66,8 @@ public class HomeController {
 		String pPasscode = hsr.getParameter("passcode");
 		System.out.println("title ["+pTitle+"] content ["+pContent+"] writer ["+pWriter+"] passcode ["+pPasscode+"]");
 		//출력완료-->insert into DB
+		iBBS bbs = sqlSession.getMapper(iBBS.class);
+		bbs.writebbs(sTitle, sContent, sWriter, sPasscode);
 		return "redirect:/list";
 	}
 //	@RequestMapping(value = "/update", method = RequestMethod.POST)
